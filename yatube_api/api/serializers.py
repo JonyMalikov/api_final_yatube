b19e769cd6c2448fe63720cbf6264af4165ec969
Евгений Маликов <malikov.evgeniy@gmail.com>
2023-04-13 23:11:41 +0300

    исправление замечанийÑ

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -49,5 +49,7 @@ class FollowSerializer(serializers.ModelSerializer):
 
     def validate_following(self, value):
         if value == self.context['request'].user:
-            raise serializers.ValidationError('Нельзя подписаться на самого себя')
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя'
+            )
         return value
